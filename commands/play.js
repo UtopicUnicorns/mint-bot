147f49209bc09f64d2e9470dc378cdc85b262eb7
UtopicUnicorns <30652988+UtopicUnicorns@users.noreply.github.com>
2019-10-08 21:05:48 +0200

    Add files via upload

--- commands/play.js
+++ commands/play.js
@@ -1,78 +1,75 @@
 const {
-	Util
+	Util,
+	Client
 } = require('discord.js');
+const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
+const youtube = new YouTube(GOOGLE_API_KEY);
 
 module.exports = {
 	name: 'play',
 	description: 'Play a song in your channel!',
 	async execute(message) {
-		const args = message.content.split(' ');
-		const queue = message.client.queue;
-		const serverQueue = message.client.queue.get(message.guild.id);
+		
 
-		const voiceChannel = message.member.voiceChannel;
-		if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
-		const permissions = voiceChannel.permissionsFor(message.client.user);
-		if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
-			return message.channel.send('I need the permissions to join and speak in your voice channel!');
+		const args = msg.content.split(' ');
+		const searchString = args.slice(1).join(' ');
+		const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
+		const serverQueue = queue.get(msg.guild.id);
+
+		const voiceChannel = msg.member.voiceChannel;
+		if (!voiceChannel) return msg.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
+		const permissions = voiceChannel.permissionsFor(msg.client.user);
+		if (!permissions.has('CONNECT')) {
+			return msg.channel.send('I cannot connect to your voice channel, make sure I have the proper permissions!');
+		}
+		if (!permissions.has('SPEAK')) {
+			return msg.channel.send('I cannot speak in this voice channel, make sure I have the proper permissions!');
 		}
 
-		const songInfo = await ytdl.getInfo(args[1]);
-		const song = {
-			title: songInfo.title,
-			url: songInfo.video_url,
-		};
-
-		if (!serverQueue) {
-			const queueContruct = {
-				textChannel: message.channel,
-				voiceChannel: voiceChannel,
-				connection: null,
-				songs: [],
-				volume: 5,
-				playing: true,
-			};
-
-			queue.set(message.guild.id, queueContruct);
-
-			queueContruct.songs.push(song);
-
-			try {
-				var connection = await voiceChannel.join();
-				queueContruct.connection = connection;
-				this.play(message, queueContruct.songs[0]);
-			} catch (err) {
-				console.log(err);
-				queue.delete(message.guild.id);
-				return message.channel.send('error');
+		if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
+			const playlist = await youtube.getPlaylist(url);
+			const videos = await playlist.getVideos();
+			for (const video of Object.values(videos)) {
+				const video2 = await youtube.getVideoByID(video.id); // eslint-disable-line no-await-in-loop
+				await handleVideo(video2, msg, voiceChannel, true); // eslint-disable-line no-await-in-loop
 			}
+			return msg.channel.send(`✅ Playlist: **${playlist.title}** has been added to the queue!`);
 		} else {
-			serverQueue.songs.push(song);
-			return message.channel.send(`${song.title} has been added to the queue!`);
+			try {
+				var video = await youtube.getVideo(url);
+			} catch (error) {
+				try {
+					var videos = await youtube.searchVideos(searchString, 10);
+					let index = 0;
+					msg.channel.send(`
+__**Song selection:**__
+${videos.map(video2 => `**${++index} -** ${video2.title}`).join('\n')}
+Please provide a value to select one of the search results ranging from 1-10.
+					`);
+					// eslint-disable-next-line max-depth
+					try {
+						var response = await msg.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11, {
+							maxMatches: 1,
+							time: 10000,
+							errors: ['time']
+						});
+					} catch (err) {
+						console.error(err);
+						return msg.channel.send('No or invalid value entered, cancelling video selection.');
+					}
+					const videoIndex = parseInt(response.first().content);
+					var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
+				} catch (err) {
+					console.error(err);
+					return msg.channel.send('🆘 I could not obtain any search results.');
+				}
+			}
+			return handleVideo(video, msg, voiceChannel);
 		}
-	},
 
-	play(message, song) {
-		const queue = message.client.queue;
-		const guild = message.guild;
-		const serverQueue = queue.get(message.guild.id);
-	
-		if (!song) {
-			serverQueue.voiceChannel.leave();
-			queue.delete(guild.id);
-			return;
-		}
-	
-		const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
-			.on('end', () => {
-				console.log('Music ended!');
-				serverQueue.songs.shift();
-				this.play(message, serverQueue.songs[0]);
-			})
-			.on('error', error => {
-				console.error(error);
-			});
-		dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+
+
+
 	}
 };
