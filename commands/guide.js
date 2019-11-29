edcee3862739074763632612d3f2a119cd9f42cf
UtopicUnicorns <30652988+UtopicUnicorns@users.noreply.github.com>
2019-11-29 19:19:33 +0100

    Yes mlads
    Yes mlads

--- commands/guide.js
+++ commands/guide.js
@@ -3,9 +3,28 @@ const fs = require("fs");
 module.exports = {
     name: 'guide',
     description: 'Guides here',
-    execute(message) {
+    async execute(message) {
         if (message.channel.id === '648911771624538112') {
             let args = message.content.slice(7).split(' ');
+
+            function sleep(ms) {
+                return new Promise(resolve => setTimeout(resolve, ms));
+            }
+            if (message.content == "!guide mint") {
+                array = ["0", "1", "2", "3", "4", "5", "6", "7"];
+                for (let i in array) {
+                    await sleep(1000);
+                    let g1 = fs.readFileSync(`./guide/6/${i}.txt`).toString();
+                    let embed = new Discord.RichEmbed()
+                        .setDescription(`${g1}`)
+                        .setColor(`RANDOM`)
+                        .setTimestamp();
+                    message.author.send({
+                        embed: embed
+                    });
+                }
+                return message.delete();
+            }
             fs.stat(`./guide/${args[0]}/${args[1]}.txt`, function(err, fileStat) {
                 if (err) {
                     if (err.code == 'ENOENT') {
