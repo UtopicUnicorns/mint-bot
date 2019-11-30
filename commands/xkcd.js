98f0e27377c5a4e9eea36b6cecd1fde96c0c9722
UtopicUnicorns <30652988+UtopicUnicorns@users.noreply.github.com>
2019-11-30 19:55:24 +0100

    Update xkcd.js

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -14,7 +14,8 @@ module.exports = {
             }, (err, res, body) => {
                 if (err) return message.channel.send(err);
                 const embed = new Discord.RichEmbed()
-                    .setDescription(body.safe_title)
+                    .setTitle(body.safe_title)
+                    .setURL(`https://xkcd.com/${body.num}/`)
                     .setColor('#RANDOM')
                     .setImage(body.img)
                     .setFooter(body.alt)
@@ -24,13 +25,35 @@ module.exports = {
             });
             return
         }
-        let url = "https://xkcd.com/info.0.json";
+        let args = message.content.slice().split(" ");
+        if (!args[1]) {
+            let url = "https://xkcd.com/info.0.json";
+            request(url, {
+                json: true
+            }, (err, res, body) => {
+                if (err) return message.channel.send(err);
+                const embed = new Discord.RichEmbed()
+                    .setTitle(body.safe_title)
+                    .setURL(`https://xkcd.com/${body.num}/`)
+                    .setColor('#RANDOM')
+                    .setImage(body.img)
+                    .setFooter(body.alt)
+                message.channel.send({
+                    embed: embed
+                });
+            });
+        }
+        let baseurl = "https://xkcd.com/";
+        let num = args[1];
+        let end = "/info.0.json";
+        let url = baseurl + num + end;
         request(url, {
             json: true
         }, (err, res, body) => {
             if (err) return message.channel.send(err);
             const embed = new Discord.RichEmbed()
-                .setDescription(body.safe_title)
+                .setTitle(body.safe_title)
+                .setURL(`https://xkcd.com/${body.num}/`)
                 .setColor('#RANDOM')
                 .setImage(body.img)
                 .setFooter(body.alt)
