e420f701f62767a2b7a4c59d24553c79dc316c9f
UtopicUnicorns <30652988+UtopicUnicorns@users.noreply.github.com>
2019-12-14 19:35:15 +0100

    .

--- commands/test2.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-const request = require("request");
-module.exports = {
-    name: 'test2',
-    description: '[admin] testing grounds2',
-    execute(message) {
-       message.channel.send("Test");
-    }
-};
