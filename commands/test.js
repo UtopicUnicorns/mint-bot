0d871f44a8d1fdd5688cf0bc788510541b31c50f
UtopicUnicorns <30652988+UtopicUnicorns@users.noreply.github.com>
2019-12-14 19:58:20 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -4,17 +4,6 @@ module.exports = {
     name: 'test',
     description: '[admin] testing grounds',
     execute(message) {
-        let baseurl = "https://some-random-api.ml/img/cat";
-        let url = baseurl;
-        request(url, {
-            json: true
-        }, (err, res, body) => {
-            if (err) return message.channel.send(err);
-            const embed = new Discord.RichEmbed()
-                .setImage(body.link)
-            message.channel.send({
-                embed: embed
-            });
-        });
+        message.channel.send("owo");
     }
 };
