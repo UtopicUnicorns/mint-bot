f1d9e508b3d099d27a8d3617f73abaea6d4b3344
UtopicUnicorns <30652988+UtopicUnicorns@users.noreply.github.com>
2019-12-19 23:47:16 +0100

    Cleaning
    Cleaning

--- commands/test.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-const request = require("request");
-module.exports = {
-    name: 'test',
-    description: '[admin] testing grounds',
-    execute(message) {
-        message.channel.send("owo");
-    }
-};
