831f2add9ac37c529c3129f7273d803fd38bb25a
UtopicUnicorns <30652988+UtopicUnicorns@users.noreply.github.com>
2020-01-27 20:54:56 +0100

    stuff

--- commands/embed.js
+++ commands/embed.js
@@ -5,19 +5,13 @@ module.exports = {
     execute(message) {
         if (!message.member.hasPermission('KICK_MEMBERS')) return;
             let args = message.content.slice(7).split('\n');
-            let element = `${args[0]}`;
-            let str = "";
-            for (let i of args) {
-                if (i != element)
-                    str += i + "\n";
-            }
             message.delete();
             let embed = new Discord.RichEmbed()
                 .setColor(`RANDOM`)
                 .setTitle(args[0])
-                .setDescription(str)
+                .setDescription(args)
             return message.channel.send({
-                embed: embed
+                embed
             });
     },
 };
